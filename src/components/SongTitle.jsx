1b08db5b8677fc838f2fab780790694fb5451b44
michaellgans <michaellgans@Victoria>
2024-09-06 13:01:05 -0500

    MLG - 1:00 PM - Creates SongTitle.jsx and imports it into App.jsx"

---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,20 @@
+// SongTitle Component
+
+// Asset Imports
+
+// Script Imports
+
+// Returns a SongTitle Component
+export function SongTitle() {
+    // Define Hook
+    return (
+        <div className="song-text">
+            <h1 className="song-title">
+                Song Title
+            </h1>
+            <div className="artist">
+                Artist Name
+            </div>
+        </div>
+    );
+}
