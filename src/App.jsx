3bad8abbffd4c005df2412701cac2a8d92870d0e
michaellgans <michaellgans@Victoria>
2024-09-06 12:44:58 -0500

    MLG - 12:44 PM - Creates CoverArt.jsx and imports it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import MusicPlayer from "./MusicPlayer";
+import { CoverArt } from "./components/CoverArt";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <MusicPlayer />
+      <CoverArt />
       <Footer />
     </div>
   );
