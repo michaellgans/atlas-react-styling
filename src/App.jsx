50d398577feea7da833655ba98b83dee27225da6
michaellgans <michaellgans@Victoria>
2024-09-06 13:37:26 -0500

    MLG - 1:37 PM - imports VolumeControl.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MusicPlayer from "./MusicPlayer";
 import { CoverArt } from "./components/CoverArt";
 import { SongTitle } from "./components/SongTitle";
 import { PlayControls } from "./components/PlayControls";
+import { VolumeControl } from "./components/VolumeControl";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <CoverArt />
       <SongTitle />
       <PlayControls />
+      <VolumeControl />
       <Footer />
     </div>
   );
