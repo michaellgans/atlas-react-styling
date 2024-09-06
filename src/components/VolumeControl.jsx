2f684728fbbc41b2aaf6cda497736885596e04d5
michaellgans <michaellgans@Victoria>
2024-09-06 13:36:33 -0500

    MLG - 1:36 PM - Creates VolumeControl.jsx"

---
+++ src/components/VolumeControl.jsx
@@ -0,0 +1,16 @@
+// VolumeControl Component
+
+// Asset Imports
+
+// Script Imports
+
+// Returns a VolumeControl Component
+export function VolumeControl() {
+  // Define Hook
+  return (
+    <div className="volume-control-container">
+      <div className="volume-icon">Volume</div>
+      <div className="volume-slider">Volume Sliiiiiiiiiider</div>
+    </div>
+  );
+}
