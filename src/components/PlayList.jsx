a7379035335b932f419c1ea2d0120ded6866c942
michaellgans <michaellgans@Victoria>
2024-09-07 18:09:18 -0500

    MLG - 6:09 PM - Styles playlist container

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -9,8 +9,13 @@ import { PlayListItem } from "./PlayListItem";
 export function PlayList() {
   // Define Hook
   return (
-    <div className="play-list p-6">
-      <PlayListItem />
+    <div className="play-list w-full border-l p-6">
+      <h3 className="play-list-title mb-4 text-lg font-semibold">Playlist</h3>
+      <div className="all-songs flex flex-col justify-between pr-4">
+        <PlayListItem />
+        <PlayListItem />
+        <PlayListItem />
+      </div>
     </div>
   );
 }
