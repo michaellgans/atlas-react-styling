bd65ead4965241103c03ad52acfd3ba70f2aada5
michaellgans <michaellgans@Victoria>
2024-09-07 09:41:34 -0500

    MLG - 9:41 AM - creates CurrentlyPlaying.jsx

---
+++ src/components/CurrentlyPlaying.jsx
@@ -0,0 +1,22 @@
+// CurrentlyPlaying Component
+
+// Asset Imports
+import { CoverArt } from "./CoverArt";
+import { PlayControls } from "./PlayControls";
+import { SongTitle } from "./SongTitle";
+import { VolumeControl } from "./VolumeControl";
+
+// Script Imports
+
+// Returns a CurrentlyPlaying Component
+export function CurrentlyPlaying() {
+    // Define Hook
+    return (
+        <div className="currently-playing">
+            <CoverArt />
+            <SongTitle />
+            <PlayControls />
+            <VolumeControl />
+        </div>
+    );
+}
