f6e2d31f4b7effbe7c00ca2a6eb8b403c6909ddf
michaellgans <michaellgans@Victoria>
2024-09-07 19:18:39 -0500

    MLG - 7:18 PM - adjusts text size of playlist items

--- src/components/PlayListItem.jsx
+++ src/components/PlayListItem.jsx
@@ -8,7 +8,7 @@
 export function PlayListItem() {
   // Define Hook
   return (
-    <div className="play-list-item mb-1 flex h-10 items-center justify-between rounded-md bg-slate-100 text-xs font-medium">
+    <div className="play-list-item mb-1 flex h-10 items-center justify-between rounded-md bg-slate-100 text-sm font-medium">
       <div className="song-info">
         <div className="song-title pb-0.5">Song Title</div>
         <div className="genre text-slate-500">Genre</div>
