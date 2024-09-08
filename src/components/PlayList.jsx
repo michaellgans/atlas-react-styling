bd8ba4d48a32b540c013f3ccb73d8bffcc205aba
michaellgans <michaellgans@Victoria>
2024-09-08 12:19:55 -0500

    MLG - 12:17 PM - Adds styling to border

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -9,7 +9,7 @@ import { PlayListItem } from "./PlayListItem";
 export function PlayList() {
   // Define Hook
   return (
-    <div className="play-list w-full p-6 sm:w-1/2 sm:border-l">
+    <div className="play-list w-full p-6 sm:w-1/2 sm:border-l sm:border-muted-text">
       <h3 className="play-list-title mb-4 text-xl font-semibold">Playlist</h3>
       <div className="all-songs flex flex-col pr-4">
         <PlayListItem />
